2cf9c97f26e9abd7735fab6396511279257026ef
Tran Ngoc Tuan Anh <anhtnt@elightup.com>
2024-02-29 11:09:37 +0700

    Update login-account.php

--- wp/demo/login-account.php
+++ wp/demo/login-account.php
@@ -1,10 +1,10 @@
 <?php
 add_filter( 'login_message', function( $message ) {
-        $login = '<p>Use the account below to access the demo</p>';
-        $login .= '<p>Username: demo</p>';
-        $login .= '<p>Password: demo</p>';
+	$login = '<p>Use the account below to access the demo</p>';
+	$login .= '<p>Username: demo</p>';
+	$login .= '<p>Password: demo</p>';
 
-        $login = wp_get_admin_notice( $login, ['type' => 'info', 'paragraph_wrap' => false] );
+	$login = wp_get_admin_notice( $login, ['type' => 'info', 'paragraph_wrap' => false] );
 
-        return $message . $login;
+	return $message . $login;
 } );
