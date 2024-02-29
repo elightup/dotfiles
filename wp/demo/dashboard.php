18a45dd68a7c425fe350f3db6825eac389bc08a6
Tran Ngoc Tuan Anh <anhtnt@elightup.com>
2024-02-29 11:03:49 +0700

    Create dashboard.php

---
+++ wp/demo/dashboard.php
@@ -0,0 +1,115 @@
+<?php
+add_action( 'welcome_panel', function() {
+	?>
+	<div class="welcome-panel-content">
+		<div class="welcome-panel-header">
+			<div class="welcome-panel-header-image">
+				<svg preserveAspectRatio="xMidYMin slice" fill="none" viewBox="0 0 1232 240" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+					<g clip-path="url(#a)">
+						<path fill="#151515" d="M0 0h1232v240H0z"></path>
+						<ellipse cx="616" cy="232" fill="url(#b)" opacity=".05" rx="1497" ry="249"></ellipse>
+						<mask id="d" width="1000" height="400" x="232" y="20" maskUnits="userSpaceOnUse" style="mask-type:alpha">
+						<path fill="url(#c)" d="M0 0h1000v400H0z" transform="translate(232 20)"></path>
+						</mask>
+						<g stroke-width="2" mask="url(#d)">
+						<path stroke="url(#e)" d="M387 20v1635"></path>
+						<path stroke="url(#f)" d="M559.5 20v1635"></path>
+						<path stroke="url(#g)" d="M732 20v1635"></path>
+						<path stroke="url(#h)" d="M904.5 20v1635"></path>
+						<path stroke="url(#i)" d="M1077 20v1635"></path>
+						</g>
+					</g>
+					<defs>
+						<linearGradient id="e" x1="387.5" x2="387.5" y1="20" y2="1655" gradientUnits="userSpaceOnUse">
+						<stop stop-color="#3858E9" stop-opacity="0"></stop>
+						<stop offset=".297" stop-color="#3858E9"></stop>
+						<stop offset=".734" stop-color="#3858E9"></stop>
+						<stop offset="1" stop-color="#3858E9" stop-opacity="0"></stop>
+						<stop offset="1" stop-color="#3858E9" stop-opacity="0"></stop>
+						</linearGradient>
+						<linearGradient id="f" x1="560" x2="560" y1="20" y2="1655" gradientUnits="userSpaceOnUse">
+						<stop stop-color="#FFFCB5" stop-opacity="0"></stop>
+						<stop offset="0" stop-color="#FFFCB5" stop-opacity="0"></stop>
+						<stop offset=".297" stop-color="#FFFCB5"></stop>
+						<stop offset=".734" stop-color="#FFFCB5"></stop>
+						<stop offset="1" stop-color="#FFFCB5" stop-opacity="0"></stop>
+						</linearGradient>
+						<linearGradient id="g" x1="732.5" x2="732.5" y1="20" y2="1655" gradientUnits="userSpaceOnUse">
+						<stop stop-color="#C7FFDB" stop-opacity="0"></stop>
+						<stop offset=".297" stop-color="#C7FFDB"></stop>
+						<stop offset=".693" stop-color="#C7FFDB"></stop>
+						<stop offset="1" stop-color="#C7FFDB" stop-opacity="0"></stop>
+						</linearGradient>
+						<linearGradient id="h" x1="905" x2="905" y1="20" y2="1655" gradientUnits="userSpaceOnUse">
+						<stop stop-color="#FFB7A7" stop-opacity="0"></stop>
+						<stop offset=".297" stop-color="#FFB7A7"></stop>
+						<stop offset=".734" stop-color="#FFB7A7"></stop>
+						<stop offset="1" stop-color="#3858E9" stop-opacity="0"></stop>
+						<stop offset="1" stop-color="#FFB7A7" stop-opacity="0"></stop>
+						</linearGradient>
+						<linearGradient id="i" x1="1077.5" x2="1077.5" y1="20" y2="1655" gradientUnits="userSpaceOnUse">
+						<stop stop-color="#7B90FF" stop-opacity="0"></stop>
+						<stop offset=".297" stop-color="#7B90FF"></stop>
+						<stop offset=".734" stop-color="#7B90FF"></stop>
+						<stop offset="1" stop-color="#3858E9" stop-opacity="0"></stop>
+						<stop offset="1" stop-color="#7B90FF" stop-opacity="0"></stop>
+						</linearGradient>
+						<radialGradient id="b" cx="0" cy="0" r="1" gradientTransform="matrix(0 249 -1497 0 616 232)" gradientUnits="userSpaceOnUse">
+						<stop stop-color="#3858E9"></stop>
+						<stop offset="1" stop-color="#151515" stop-opacity="0"></stop>
+						</radialGradient>
+						<radialGradient id="c" cx="0" cy="0" r="1" gradientTransform="matrix(0 765 -1912.5 0 500 -110)" gradientUnits="userSpaceOnUse">
+						<stop offset=".161" stop-color="#151515" stop-opacity="0"></stop>
+						<stop offset=".682"></stop>
+						</radialGradient>
+						<clipPath id="a">
+						<path fill="#fff" d="M0 0h1232v240H0z"></path>
+						</clipPath>
+					</defs>
+				</svg>
+			</div>
+			<h2>Welcome to Slim SEO Demo!</h2>
+			<p>On this website, you'll find how to use Slim SEO and all of its premium extensions.</p>
+			<p>If you have any questions or feedback, please <a href="https://wpslimseo.com/contact/" href="_blank">contact us</a>. Have a great time with Slim SEO!</p>
+		</div>
+		<div class="welcome-panel-column-container">
+			<div class="welcome-panel-column">
+				<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+					<rect width="48" height="48" rx="4" fill="#CFCABE" style="--darkreader-inline-fill: #38352f;" data-darkreader-inline-fill=""></rect>
+					<path d="M23 34v-4h-5l-2.293-2.293a1 1 0 0 1 0-1.414L18 24h5v-2h-7v-6h7v-2h2v2h5l2.293 2.293a1 1 0 0 1 0 1.414L30 22h-5v2h7v6h-7v4h-2Zm-5-14h11.175l.646-.646a.5.5 0 0 0 0-.708L29.175 18H18v2Zm.825 8H30v-2H18.825l-.646.646a.5.5 0 0 0 0 .708l.646.646Z" fill="#151515" style="--darkreader-inline-fill: #b1afac;" data-darkreader-inline-fill=""></path>
+				</svg>
+				<div>
+					<h3>Getting Started</h3>
+					<p>Start to expore <a href="/wp-admin/options-general.php?page=slim-seo">Slim SEO settings</a> to understant what features the plugin offers. Most of the settings are set up automatically for you.</p>
+					<p>Start <a href="https://demo.wpslimseo.com/wp-admin/post.php?post=1&action=edit">edit a post</a> to see how the meta title and description are auto generated. Don't forget to check <a href="https://demo.wpslimseo.com/leverage-chrome-free-vpn-for-your-startup-in-this-digital-era/" target="_blank">the front end</a> to see the them in action.</a>
+				</div>
+			</div>
+			<div class="welcome-panel-column">
+				<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+					<rect width="48" height="48" rx="4" fill="#CFCABE" style="--darkreader-inline-fill: #38352f;" data-darkreader-inline-fill=""></rect>
+					<path d="M25.7781 16.8569L25.8 22.8573L28.9984 22.8572C29.805 22.8572 30.2796 23.6339 29.8204 24.2024L23.8213 31.6292C23.2633 32.3201 22.2013 31.9819 22.2013 31.1416L22.2 25.1481H19.0016C18.1961 25.1481 17.7212 24.3733 18.1782 23.8047L24.1496 16.3722C24.7055 15.6804 25.7749 16.0169 25.7781 16.8569Z" fill="#151515" style="--darkreader-inline-fill: #b1afac;" data-darkreader-inline-fill=""></path>
+				</svg>
+				<div>
+					<h3>Experience</h3>
+					<p>Below are some things you can try with Slim SEO:</p>
+					<p>1. Preview the <a href="/sitemap.xml" target="_blank">XML sitemap</a>.</p>
+					<p>2. <a href="https://demo.wpslimseo.com/wp-admin/options-general.php?page=slim-seo#redirection">Add a redirect</a>.</p>
+					<p>3. <a href="https://demo.wpslimseo.com/wp-admin/options-general.php?page=slim-seo#code">Add some codes to header and footer</a> (like Google Analytics, Facebook Pixel, live chat, etc.).</p>
+				</div>
+			</div>
+			<div class="welcome-panel-column">
+				<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+					<rect width="48" height="48" rx="4" fill="#CFCABE" style="--darkreader-inline-fill: #38352f;" data-darkreader-inline-fill=""></rect>
+					<path d="M24 18.285C23.55 18.285 23.1638 18.1237 22.8413 17.8012C22.5188 17.4788 22.3575 17.0925 22.3575 16.6425C22.3575 16.1925 22.5188 15.8062 22.8413 15.4837C23.1638 15.1612 23.55 15 24 15C24.45 15 24.8363 15.1612 25.1588 15.4837C25.4813 15.8062 25.6425 16.1925 25.6425 16.6425C25.6425 17.0925 25.4813 17.4788 25.1588 17.8012C24.8363 18.1237 24.45 18.285 24 18.285ZM21.5925 33V21.0075C20.5725 20.9325 19.5863 20.8275 18.6338 20.6925C17.6813 20.5575 16.77 20.385 15.9 20.175L16.2375 18.825C17.5125 19.125 18.78 19.3387 20.04 19.4662C21.3 19.5938 22.62 19.6575 24 19.6575C25.38 19.6575 26.7 19.5938 27.96 19.4662C29.22 19.3387 30.4875 19.125 31.7625 18.825L32.1 20.175C31.23 20.385 30.3187 20.5575 29.3663 20.6925C28.4137 20.8275 27.4275 20.9325 26.4075 21.0075V33H25.0575V27.15H22.9425V33H21.5925Z" fill="#151515" style="--darkreader-inline-fill: #b1afac;" data-darkreader-inline-fill=""></path>
+				</svg>
+				<div>
+					<h3>Premium features</h3>
+					<p><a href="https://demo.wpslimseo.com/wp-admin/options-general.php?page=slim-seo#schema">Build your own schemas</a> and preview them on the front end.</p>
+					<p><a href="https://demo.wpslimseo.com/wp-admin/post.php?post=1&action=edit">Edit a post</a> and add links to the content and see how Slim SEO <a href="https://demo.wpslimseo.com/wp-admin/options-general.php?page=slim-seo#link-manager">analyzes and reports</a> the links for you.</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php
+} );
